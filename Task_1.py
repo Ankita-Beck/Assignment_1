93c40871203af12cdc35cea96296a3645d1983f3
Ankita-Beck <anneankita8@gmail.com>
2023-09-07 12:24:40 +0530

    Arithmetic operations

---
+++ Task_1.py
@@ -0,0 +1,19 @@
+# Arithmetic operations
+a = 15
+b = 45
+
+# Addition
+add = (a+b)
+print("The addition of {} and {} is {}".format(a,b,add))
+
+# Subtraction
+sub = (b-a)
+print("The subraction of {} and {} is {}".format(b,a,sub))
+
+# Multiplication
+mul = (a*b)
+print("The multiplication of {} and {} is {}".format(a,b,mul) )
+
+# Division
+div = (a%b)
+print("The division of {} and {} is {}" .format(a,b,div))
